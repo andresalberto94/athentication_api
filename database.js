f85aa5cd07bdffd9f230cbfcb91adc58fb57f10d
Andrés Gómez <69098107+andresalberto94@users.noreply.github.com>
2024-03-25 21:40:43 +0200

    Update database.js

--- database.js
+++ database.js
@@ -3,13 +3,5 @@ require('dotenv').config();
 
 
 
-const pool = new Pool({
-    user: process.env.DB_USERPOSTGRES_USER,
-    host: process.env.PGHOST,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.PGPORT,
-    
-  });
 
   module.exports = pool;
